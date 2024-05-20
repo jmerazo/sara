c45503623c908576a64de9536a7bb747ccc88a35
jmerazo <55777009+jmerazo@users.noreply.github.com>
2024-05-19 23:35:33 -0500

    nurseries

--- catalogo/nurseries/nurseries.py
+++ catalogo/nurseries/nurseries.py
@@ -9,13 +9,15 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Nurseries, UserNurseries
 
 class NurseriesView(APIView):
-    def get(self, request, format=None): 
+    def get(self, request, format=None):
         query = """
-        SELECT uv.id, uv.vivero_id, uv.especie_forestal_id, ef.nom_comunes, ef.nombre_cientifico_especie, ef.nombre_autor_especie, uv.tipo_venta, uv.unidad_medida, uv.cantidad_stock, uv.ventas_realizadas, uv.observaciones, uv.fecha_registro, uv.fecha_actualizacion, uv.activo, v.nombre_vivero, v.nit, v.representante_legal_id, u.first_name, u.last_name, v.ubicacion, v.email, v.telefono
+        SELECT uv.id, uv.vivero_id, uv.especie_forestal_id, ef.nom_comunes, ef.nombre_cientifico_especie, ef.nombre_autor_especie, uv.tipo_venta, uv.unidad_medida, uv.cantidad_stock, uv.ventas_realizadas, uv.observaciones, uv.fecha_registro, uv.fecha_actualizacion, uv.activo, v.nombre_vivero, v.nit, v.representante_legal_id, u.first_name, u.last_name, v.ubicacion, v.email, v.telefono, d.name AS departamento, c.name AS municipio
         FROM UserEspecieForestal AS uv
         INNER JOIN viveros AS v ON v.id = uv.vivero_id
         INNER JOIN especie_forestal AS ef ON ef.cod_especie = uv.especie_forestal_id
-        INNER JOIN Users AS u ON u.id = v.representante_legal_id;
+        INNER JOIN Users AS u ON u.id = v.representante_legal_id
+        INNER JOIN departments AS d ON d.id = v.department
+        INNER JOIN cities AS c ON c.id = v.city;
         """
 
         with connection.cursor() as cursor:
@@ -23,10 +25,45 @@ class NurseriesView(APIView):
             results = cursor.fetchall()
             columns = [col[0] for col in cursor.description]
 
-        # Crear una lista de diccionarios, cada uno representando una fila del resultado
-        queryset = [{columns[index]: value for index, value in enumerate(row)} for row in results]
+        # Procesar los resultados para agrupar las especies por vivero
+        viveros = {}
+        for row in results:
+            row_dict = {columns[index]: value for index, value in enumerate(row)}
+            vivero_id = row_dict['vivero_id']
 
-        return Response(queryset)
+            if vivero_id not in viveros:
+                viveros[vivero_id] = {
+                    'id': row_dict['id'],
+                    'vivero_id': row_dict['vivero_id'],
+                    'nombre_vivero': row_dict['nombre_vivero'],
+                    'nit': row_dict['nit'],
+                    'representante_legal_id': row_dict['representante_legal_id'],
+                    'first_name': row_dict['first_name'],
+                    'last_name': row_dict['last_name'],
+                    'ubicacion': row_dict['ubicacion'],
+                    'email': row_dict['email'],
+                    'telefono': row_dict['telefono'],
+                    'departamento': row_dict['departamento'],
+                    'municipio': row_dict['municipio'],
+                    'activo': row_dict['activo'],
+                    'especies': []
+                }
+            
+            especie = {
+                'especie_forestal_id': row_dict['especie_forestal_id'],
+                'nom_comunes': row_dict['nom_comunes'],
+                'nombre_cientifico_especie': row_dict['nombre_cientifico_especie'],
+                'nombre_autor_especie': row_dict['nombre_autor_especie'],
+                'tipo_venta': row_dict['tipo_venta'],
+                'unidad_medida': row_dict['unidad_medida'],
+                'cantidad_stock': row_dict['cantidad_stock'],
+                'ventas_realizadas': row_dict['ventas_realizadas'],
+                'observaciones': row_dict['observaciones']
+            }
+
+            viveros[vivero_id]['especies'].append(especie)
+
+        return Response(list(viveros.values()))
 
     def post(self, request, format=None):
         serializer = NurseriesSerializer(data=request.data)
