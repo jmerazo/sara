bd1bd099c95118fba969550ccec29de6b312d4a4
jmerazo <55777009+jmerazo@users.noreply.github.com>
2024-08-12 07:41:58 -0500

    fixed endpoint monitoring

--- catalogo/monitorings/serializers.py
+++ catalogo/monitorings/serializers.py
@@ -1,16 +1,36 @@
 from rest_framework import serializers
 from .models import Monitorings
+from ..candidates.models import CandidatesTrees
+from ..models import Users
 from ..candidates.serializers import CandidateTreesSerializer
 from ..users.serializers import UsersSerializer
+from ..species.models import SpecieForrest
+from ..species.serializers import SpecieForrestSerializer
 
 class MonitoringTreesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Monitorings
         fields = ['id', 'evaluacion', 'fecha_monitoreo', 'usuario_realiza_monitoreo']
 
+class CandidateTreesMonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CandidatesTrees
+        fields = ['id', 'numero_placa', 'cod_expediente', 'cod_especie_id']
+
+class UsersMonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ['id', 'first_name', 'last_name']
+
+class SpecieForrestMonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SpecieForrest
+        fields = ['id', 'habit', 'vernacularName', 'nombre_cientifico']
+
 class MonitoringsSerializer(serializers.ModelSerializer):
-    evaluacion = CandidateTreesSerializer()
-    user = UsersSerializer()
+    evaluacion = CandidateTreesMonSerializer()
+    user = UsersMonSerializer()
+    specie = SpecieForrestMonSerializer(source='evaluacion.cod_especie')
 
     class Meta:
         model = Monitorings
--- sara/settings.py
+++ sara/settings.py
@@ -180,6 +180,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# settings.py
+
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
@@ -192,6 +194,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ], 
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12,  # Número de resultados por página
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
